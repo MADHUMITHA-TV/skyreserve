0433fab95056e5a1ebae081498285f7ae0e59ec1
MADHUMITHA-TV <madhumithavijayan52@gmail.com>
2026-07-14 17:13:53 +0530

    Add JWT debug logging

--- backend/src/modules/auth/jwt.util.js
+++ backend/src/modules/auth/jwt.util.js
@@ -1,6 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (user) => {
+  console.log("NODE_ENV:", process.env.NODE_ENV);
+  console.log("JWT_ACCESS_SECRET:", process.env.JWT_ACCESS_SECRET);
+  console.log("JWT_REFRESH_SECRET:", process.env.JWT_REFRESH_SECRET);
+
   return jwt.sign(
     {
       id: user.id,
